852d49c131833c0e16a8db39789692298d71488f
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-24 10:11:27 +0100

    Solucionado

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // Happy coding!
 
+const swatches = document.querySelector(".canvas");
+
+swatches.addEventListener("click", function (event) {
+    document.body.style.backgroundColor = window.getComputedStyle(event.target).backgroundColor;
+});
